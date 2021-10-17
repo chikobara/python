8580815718c6f4555c00f4326960b83f46948d58
Chiko SAN <anonymoussyria6@gmail.com>
2021-10-17 11:58:44 +0300

    a

--- Learn the Basics/test.py
+++ Learn the Basics/test.py
@@ -1,13 +1,5 @@
-import socket
-mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-mysock.connect(('data.pr4e.org', 80))
-cmd = 'Get http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
-mysock.send(cmd)
-
-
-while True:
-	data = mysock.recv(512)
-	if (len(data) < 1):
-		break
-	print(data.decode())
-mysock.close
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+url = input("Enter the url : ")
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
