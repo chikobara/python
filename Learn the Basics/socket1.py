c5606b7cd0e55de380b9f2b8684cc5d7da85f26a
Chikobara <anonymoussyria6@gmail.com>
2021-10-28 12:00:24 +0300

    a

--- Learn the Basics/socket1.py
+++ Learn the Basics/socket1.py
@@ -1,4 +1,4 @@
-import socket
+"""import socket
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
 cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
@@ -11,8 +11,30 @@ while True:
     print(data.decode(),end='')
 mysock.close()
 
-
 import urllib.request , urllib.parse , urllib.error
 fhand = urllib.request.urlopen('http://data.pr4e.org/intro-short.txt')
 for i in fhand:
     print(i.decode().strip())
+    
+# using urlib in python
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/intro-short.txt')
+for i in fhand:
+    print(i.decode().strip())
+
+
+import urllib.error, urllib.parse, urllib.request
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+counts = dict()
+for line in fhand:
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+print(counts)
+"""
+
+
+import urllib.request, urllib.parse , urllib.error
+import ssl
+from bs4 import BeautifulSoup
--- Learn the Basics/yu_gi_oh_helper.py
+++ Learn the Basics/yu_gi_oh_helper.py
@@ -6,6 +6,8 @@ print("\n\n\n")
 print("You can go with: Roll dice / HP calculator ")
 modechoice = input("Enter what do want: ")
 modechoice = modechoice.lower()
+myhp = 8000
+ophp = 8000
 if modechoice == "roll dice":
     print('|{:^60}|'.format("you're about to roll a dice"))
     print('\n')
@@ -13,28 +15,16 @@ if modechoice == "roll dice":
     print('\n')
     print('|{:^60}|'.format(num))
     print('\n')
-    agn = input("Would you roll a dice again? (yes/no) ")
+    agn = input("Would you roll a dice again? ( y / n ) ")
     agn = agn.lower()
-    while agn == "yes":
+    while agn == "y":
         num = random.randint(1,6)
         print('\n')
         print('|{:^60}|'.format(num))
-        agn = input("Would you roll a dice again? ")
+        agn = input("Would you roll a dice again? ( y / n ) ")
         agn = agn.lower()
-        if agn == "no":
+        if agn == "n":
             break
-
-
-#first we would def a function to change the hp
-#def myhpchange():
-#    pass
-#def ophpchange():
-#    pass
-
-myhp = 8000
-ophp = 8000
-
-
 elif modechoice == "hp calculator":
     print('|{:^60}|'.format(f"Your HP now is : {myhp}"))
     print('|{:^60}|'.format(f"Your opponent's hp is {ophp}"))
